813094de4f364b9642615c99755ebd7b7a814523
mchowdhury <muz.c98@gmail.com>
2020-11-16 20:09:18 -0500

    Added User Model

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<h1>Welcome to A-Muz-On</h1>, root);
     // window.login = login;
     // window.signup = signup;
     // window.logout = logout;
