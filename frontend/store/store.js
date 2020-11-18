750cffe5cebdcbe18f0c5588e6d81b7d3bcb527e
mchowdhury <muz.c98@gmail.com>
2020-11-18 10:09:13 -0500

    Fixed store js for deployment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import thunk from "../middlewares/thunk";
 import logger from "redux-logger";
 import RootReducer from "../reducers/root_reducer";
 
