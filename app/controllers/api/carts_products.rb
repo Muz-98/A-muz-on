896d7ba694f3f63f449a261c2679d88b51672537
mchowdhury <muz.c98@gmail.com>
2021-01-20 17:39:01 -0500

    started backend carts

--- app/controllers/api/carts_products.rb
+++ app/controllers/api/carts_products.rb
@@ -1,11 +1,19 @@
 class Api::CartsProductsController < ApplicationController
 
     def create 
+        @cart_products = CartsProducts.new(cart_products_params)
     end
 
     def update
     end
 
     def destroy 
+        
+    end
+
+    private 
+
+    def cart_products_params
+        params.require(:cartsProducts).permit(:product_id)
     end
 end
