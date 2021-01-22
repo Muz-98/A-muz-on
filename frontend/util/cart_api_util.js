9fb33c23edb9db380b3c7a745585621e9834ee7a
mchowdhury <muz.c98@gmail.com>
2021-01-22 12:07:37 -0500

    worked on utils

--- frontend/util/cart_api_util.js
+++ frontend/util/cart_api_util.js
@@ -1,15 +1,25 @@
 export const fetchCart = () => {
-
+    return $.ajax({
+        method: "GET",
+        url: "/api/cartsproducts"
+    })
 }
 
 export const updateCart = () => {
 
 }
 
-export const addToCart = () => {
-
+export const addToCart = (cartsProduct) => {
+    return $.ajax({
+        method: 'POST',
+        url: '/api/cartsproducts',
+        data: {cartsProduct}
+    })
 }
 
-export const deleteFromCart = () => {
-
+export const deleteFromCart = (cartsProductId) => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `/api/cartsproducts/${cartsProductId}`
+    })
 }
