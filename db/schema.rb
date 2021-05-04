c1af952bee6a0381a571048605c724f3c47b566d
mchowdhury <muz.c98@gmail.com>
2021-05-04 02:15:17 -0400

    created reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_071639) do
+ActiveRecord::Schema.define(version: 2021_05_04_060210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,18 @@ ActiveRecord::Schema.define(version: 2020_11_30_071639) do
     t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.integer "rating", null: false
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id", unique: true
+    t.index ["user_id"], name: "index_reviews_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
